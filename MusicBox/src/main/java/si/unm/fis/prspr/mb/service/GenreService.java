0cb27e166ac9701a4c36bde5fae63027e656a239
Tjankulov <51080695+Tjankulov@users.noreply.github.com>
2026-05-20 19:03:38 +0200

    Update GenreService.java

--- MusicBox/src/main/java/si/unm/fis/prspr/mb/service/GenreService.java
+++ MusicBox/src/main/java/si/unm/fis/prspr/mb/service/GenreService.java
@@ -1,57 +1,42 @@
 package si.unm.fis.prspr.mb.service;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import si.unm.fis.prspr.mb.entity.Genre;
 import si.unm.fis.prspr.mb.repository.GenreRepository;
 
-
 @Service
 public class GenreService {
-	// avtomatski dependancy injection
-	@Autowired
-	private GenreRepository genreRepository;
-	
-	// CRUD OPERACIJE
-	
-	// CREATE
-	public Genre createGenre(Genre genre) {
-		return genreRepository.save(genre);
+
+    private final GenreRepository genreRepository;
+
+    public GenreService(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
     }
 
+    public Genre createGenre(Genre genre) {
+        return genreRepository.save(genre);
+    }
 
-	
-	// RETRIEVE
-	// ALL
-	public List<Genre> getAllGenres() {
-		return genreRepository.findAll();
-	}
-	
-	// BY ID
-	public Genre getGenreById(int id) {
-		Optional<Genre> Genre = genreRepository.findById(id);
-		if(Genre.isPresent()) {
-			return Genre.get();
-		} else {
-			// če ne najde, vrni null
-			return null;
-		}
-	}
-	
-	
-	// UPDATE
-	public Genre updateGenre(Genre genre) {
-		return genreRepository.save(genre);
-	}
-	
-	
-	// DELETE
-	public String deleteGenre(int id) {
-		genreRepository.deleteById(id);
-		return "Zvrst je odstranjena.";
-	}
+    public List<Genre> getAllGenres() {
+        return genreRepository.findAll();
+    }
+
+    public Genre getGenreById(int id) {
+        return genreRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Zvrst z ID " + id + " ne obstaja."));
+    }
+
+    public Genre updateGenre(int id, Genre genre) {
+        Genre existing = genreRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Zvrst z ID " + id + " ne obstaja."));
+        existing.setName(genre.getName());
+        return genreRepository.save(existing);
+    }
+
+    public void deleteGenre(int id) {
+        genreRepository.deleteById(id);
+    }
 }
