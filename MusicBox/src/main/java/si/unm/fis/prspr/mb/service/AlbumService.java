d6b1af1a9fa3b53c54765bc0ab4e75d850249788
Tjankulov <51080695+Tjankulov@users.noreply.github.com>
2026-05-20 19:03:15 +0200

    Update AlbumService.java

--- MusicBox/src/main/java/si/unm/fis/prspr/mb/service/AlbumService.java
+++ MusicBox/src/main/java/si/unm/fis/prspr/mb/service/AlbumService.java
@@ -2,7 +2,6 @@ package si.unm.fis.prspr.mb.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import si.unm.fis.prspr.mb.entity.Album;
@@ -10,35 +9,36 @@ import si.unm.fis.prspr.mb.repository.AlbumRepository;
 
 @Service
 public class AlbumService {
-	// avtomatski dependancy injection
-	@Autowired
-	private AlbumRepository albumRepository;
-	
-	
-	// CRUD OPERACIJE
-	
-	// CREATE
-	public Album createAlbum(Album album) {
-		return albumRepository.save(album);
-	}
-	
-	
-	// RETRIEVE
-	// ALL
-	public List<Album> getAllAlbums() {
-		return albumRepository.findAll();
-	}
 
-	
-	// UPDATE
-	public Album updateAlbum(Album album) {
-		return albumRepository.save(album);
-	}
-	
-	
-	// DELETE
-	public String deleteAlbum(int id) {
-		albumRepository.deleteById(id);
-		return "Album je odstranjen.";
-	}
+    private final AlbumRepository albumRepository;
+
+    public AlbumService(AlbumRepository albumRepository) {
+        this.albumRepository = albumRepository;
+    }
+
+    public Album createAlbum(Album album) {
+        return albumRepository.save(album);
+    }
+
+    public List<Album> getAllAlbums() {
+        return albumRepository.findAll();
+    }
+
+    public Album getAlbumById(int id) {
+        return albumRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Album z ID " + id + " ne obstaja."));
+    }
+
+    public Album updateAlbum(int id, Album album) {
+        Album existing = albumRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Album z ID " + id + " ne obstaja."));
+        existing.setName(album.getName());
+        existing.setGenre(album.getGenre());
+        existing.setMusician(album.getMusician());
+        return albumRepository.save(existing);
+    }
+
+    public void deleteAlbum(int id) {
+        albumRepository.deleteById(id);
+    }
 }
