7d502f59912020f89eaa6e8cd2515571762f963e
Tjankulov <51080695+Tjankulov@users.noreply.github.com>
2026-05-20 19:04:32 +0200

    Update MusicBoxApplication.java

--- MusicBox/src/main/java/si/unm/fis/prspr/mb/MusicBoxApplication.java
+++ MusicBox/src/main/java/si/unm/fis/prspr/mb/MusicBoxApplication.java
@@ -19,7 +19,7 @@ public class MusicBoxApplication {
 	      @Override
 	      public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**").allowedOrigins("*")
-	        .allowedMethods("GET", "POST", "PUT", "DELETE");;
+	        .allowedMethods("GET", "POST", "PUT", "DELETE");
 	      }
 	    };
 	}
