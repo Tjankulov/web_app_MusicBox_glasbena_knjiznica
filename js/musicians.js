2ff561dff50f899dab03e0260c9ad6a71a09dd96
Tjankulov <51080695+Tjankulov@users.noreply.github.com>
2026-05-21 18:28:57 +0200

    Update musicians.js

--- js/musicians.js
+++ js/musicians.js
@@ -1,5 +1,7 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
+const API = "https://web-app-musicbox-glasbena-knjiznica-1.onrender.com";
+
 createApp({
   data() {
     return {
@@ -9,76 +11,81 @@ createApp({
       currentPage: 0,
       totalPages: 0,
       pageSize: 10,
+
       formMusician: {
         id: null,
         name: '',
-        genre: {
-          name: '',
-        }
-      },
-      options: []
+        genre: null
+      }
     }
   },
+
   created() {
     this.loadMusicians();
     this.loadGenres();
-    this.loadMusiciansByGenre();
   },
+
   methods: {
-    // vrne seznam glasbenikov (plus paginacija)
+
     loadMusicians(page = 0) {
-      axios.get("https://web-app-musicbox-glasbena-knjiznica-1.onrender.com/musicians?page=" + page + "&size=" + this.pageSize)
+      axios.get(`${API}/musicians?page=${page}&size=${this.pageSize}`)
         .then((response) => {
           this.musicians = response.data.content;
           this.currentPage = response.data.number;
           this.totalPages = response.data.totalPages;
         })
-        .catch((error) => console.error(error));
+        .catch(console.error);
     },
+
     loadGenres() {
-      axios.get("http://localhost:8080/genres/getAll")
+      axios.get(`${API}/genres`)
         .then((response) => {
           this.genres = response.data;
         })
-        .catch((error) => console.error(error));
+        .catch(console.error);
     },
+
     loadMusiciansByGenre() {
-      axios.get("http://localhost:8080/musicians/byGenre/" + this.inputGenre)
+      axios.get(`${API}/musicians/byGenre/${this.inputGenre}`)
         .then((response) => {
           this.musicians = response.data;
         })
-        .catch((error) => console.error(error));
+        .catch(console.error);
     },
-    // za formular, kjer uporabnik vnese podatke za novega glasbenika
-    postMusician() {
-      axios.post("http://localhost:8080/musicians/create", this.formMusician)
-        .then((response) => {
-          this.loadMusicians();
-          this.formMusician.name = '';
-          this.formMusician.genre = null;
-        })
-        .catch((error) => console.error(error));
-    },
-    deleteMusician(id) {
-      axios.delete("http://localhost:8080/musicians/delete/" + id)
-        .then((response) => {
-          this.loadMusicians();
-        })
-        .catch((error) => console.error(error));
-    },
-    // s klikom na polje v tabeli, se izpolni tudi formular z enakimi podatki, ki ga lahko spremenimo
-    populateForm(musician) {
-      this.formMusician.id = musician.id;
-      this.formMusician.name = musician.name;
-      this.formMusician.genre = musician.genre;
-    },
-    // funkcija počisti obrazec, dodali smo ga k funkciji PostMusician, da se obrazec počisti, potem ko se shrani nov glasbenik
-    cleanForm() {
-      this.formMusician.id = null;
-      this.formMusician.name = '';
-      this.formMusician.genre = '';
 
+    postMusician() {
+      axios.post(`${API}/musicians`, this.formMusician)
+        .then(() => {
+          this.loadMusicians(this.currentPage);
+          this.cleanForm();
+        })
+        .catch(console.error);
     },
+
+    deleteMusician(id) {
+      axios.delete(`${API}/musicians/${id}`)
+        .then(() => {
+          this.loadMusicians(this.currentPage);
+        })
+        .catch(console.error);
+    },
+
+    populateForm(musician) {
+      this.formMusician = {
+        id: musician.id,
+        name: musician.name,
+        genre: musician.genre
+      };
+    },
+
+    cleanForm() {
+      this.formMusician = {
+        id: null,
+        name: '',
+        genre: null
+      };
+    },
+
     nextPage() {
       if (this.currentPage < this.totalPages - 1) {
         this.loadMusicians(this.currentPage + 1);
@@ -89,6 +96,7 @@ createApp({
       if (this.currentPage > 0) {
         this.loadMusicians(this.currentPage - 1);
       }
-    },
+    }
   }
+
 }).mount('#musicians');
