7f95613beff19208f07f610ce0a14d718c9c6a72
Tjankulov <51080695+Tjankulov@users.noreply.github.com>
2026-05-21 13:21:19 +0200

    Update albums.js

--- js/albums.js
+++ js/albums.js
@@ -27,14 +27,14 @@ createApp({
     methods: {
         // vrne seznam glasbenikov
         loadAlbums() {
-            axios.get("http://localhost:8080/albums/getAll")
+            axios.get("https://web-app-musicbox-glasbena-knjiznica-1.onrender.com/albums")
                 .then((response) => {
                     this.albums = response.data;
                 })
                 .catch((error) => console.error(error));
         },
         loadMusicians() {
-            axios.get("http://localhost:8080/musicians/getAll")
+            axios.get("https://web-app-musicbox-glasbena-knjiznica-1.onrender.com/musicians")
                 // arrow notation: response, ki mi ga vrne axios.get, mi omogoča da preberem podatke, ki me zanimajo (response.data) in jih vrnem v lokalno spremenljivko 'musicians' (kot bi mi napisali notri)
                 .then((response) => {
                     this.musicians = response.data;
@@ -42,7 +42,7 @@ createApp({
                 .catch((error) => console.error(error));
         },
         loadGenres() {
-            axios.get("http://localhost:8080/genres/getAll")
+            axios.get("https://web-app-musicbox-glasbena-knjiznica-1.onrender.com/genres")
                 .then((response) => {
                     this.genres = response.data;
                 })
@@ -50,17 +50,24 @@ createApp({
         },
         // za formular, kjer uporabnik vnese podatke za nov album
         postAlbum() {
-            axios.post("http://localhost:8080/albums/create", this.formAlbum)
-                .then((response) => {
-                    this.loadAlbums();
-                    this.formMusician.name = '';
-                    this.formMusician.genre = null;
-                    this.formMusician.musician = null;
-                })
-                .catch((error) => console.error(error));
+            if (this.formAlbum.id) {
+                axios.put("https://web-app-musicbox-glasbena-knjiznica-1.onrender.com/albums/" + this.formAlbum.id, this.formAlbum)
+                    .then((response) => {
+                        this.loadAlbums();
+                        this.cleanForm();
+                    })
+                    .catch((error) => console.error(error));
+            } else {
+                axios.post("https://web-app-musicbox-glasbena-knjiznica-1.onrender.com/albums", this.formAlbum)
+                    .then((response) => {
+                        this.loadAlbums();
+                        this.cleanForm();
+                    })
+                    .catch((error) => console.error(error));
+            }
         },
         deleteAlbum(id) {
-            axios.delete("http://localhost:8080/albums/delete/" + id)
+            axios.delete("https://web-app-musicbox-glasbena-knjiznica-1.onrender.com/albums/" + id)
                 .then((response) => {
                     this.loadAlbums();
                 })
@@ -70,15 +77,15 @@ createApp({
         populateForm(album) {
             this.formAlbum.id = album.id;
             this.formAlbum.name = album.name;
-            this.formAlbum.genre = album.genre;
-            this.formAlbum.musician = album.musician;
+            this.formAlbum.genre = this.genres.find(g => g.id === album.genre.id);
+            this.formAlbum.musician = this.musicians.find(m => m.id === album.musician.id);
         },
         // funkcija počisti obrazec, dodali smo ga k funkciji PostAlbum, da se obrazec počisti, potem ko se shrani nov glasbenik
         cleanForm() {
             this.formAlbum.id = null;
             this.formAlbum.name = '';
             this.formAlbum.genre = '';
-
+            this.formAlbum.musician = '';
         }
     }
 }).mount('#albums');
